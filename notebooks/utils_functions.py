8f0c997f62f5489889a5e613b1d4ad893e910f1f
dujm <jdu523@outlook.com>
2019-05-30 22:27:03 +0200

    update utils

--- notebooks/utils_functions.py
+++ notebooks/utils_functions.py
@@ -1,7 +1,8 @@
-
+# -*- coding: utf-8 -*-
 # Load packages
 # System packages
 import os
+import re
 import datetime
 import warnings
 warnings.simplefilter("ignore", UserWarning)
@@ -17,6 +18,7 @@ import seaborn as sns, matplotlib.pyplot as plt
 from gensim.summarization import summarize, keywords
 from gensim.models import KeyedVectors
 
+# +
 # Text analysis helper libraries for word frequency
 import nltk
 # Download stop words
@@ -27,6 +29,11 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from string import punctuation
+import snowballstemmer
+
+
+
+# -
 
 
 # Word cloud visualization libraries
@@ -36,11 +43,18 @@ from wordcloud import WordCloud, ImageColorGenerator
 from collections import Counter
 
 
+# +
 # Dimensionaly reduction libraries
 from sklearn.decomposition import PCA
 
 # Clustering library
 from sklearn.cluster import KMeans
+# -
+
+# sklearn 
+from sklearn.model_selection import cross_val_predict, StratifiedKFold, train_test_split
+from sklearn.metrics import log_loss, accuracy_score
+import scikitplot.plotters as skplt
 
 
 # Create a new folder function
@@ -78,8 +92,19 @@ def dm(data):
     return unique_data.sort_values(by=['unique_number','dtype'])
 
 
+# Group by a column and count the size 
+def groupby_col_count(df, colname,save_csv_dir=None):
+    df1 = df.groupby([df[colname]]) \
+        .size() \
+        .sort_values(ascending=False)
+    name= 'groupby_' +str(colname)
+    csvname = '{}.csv'.format(os.path.join(save_csv_dir,name))
+    df1.to_csv(csvname,index=False)
+    return df1.head(5)
+
+
 # Bar Plot: Count by colname
-def col_count_plot(df,colname,save_plot_dir):
+def col_count_plot(df,colname,save_plot_dir=None):
     '''
     df: dataframe
     colname: column name
@@ -101,7 +126,7 @@ def col_count_plot(df,colname,save_plot_dir):
 
 
 # Frequency plot of a col
-def frequency_plot(df,colname,save_plot_dir):
+def frequency_plot(df,colname,save_plot_dir=None):
     '''
     df: dataframe
     colname: column name
@@ -130,6 +155,13 @@ def get_average_vector(text,stop_words):
     tokens = [w.lower() for w in word_tokenize(text) if w.lower() not in stop_words]
     return np.mean(np.array([model.wv[w] for w in tokens if w in model]), axis=0)
 
+custom_words = ["fig", "figure", "et", "al", "al.", "also",
+                "data", "analyze", "study", "table", "using",
+                "method", "result", "conclusion", "author", 
+                "find", "found", "show", '"', "’", "“", "”"]
+stop_words = set(stopwords.words('english') + list(punctuation) + custom_words)
+
+
 # Build a corpus for a Text column grouped by Target columns
 def build_corpus(df,target,text,stop_words,wordnet_lemmatizer):
     '''
@@ -147,7 +179,7 @@ def build_corpus(df,target,text,stop_words,wordnet_lemmatizer):
 
 
 # World frequency plot
-def word_cloud_plot_no_mask(corpus,save_plot_dir):
+def word_cloud_plot_no_mask(corpus,save_plot_dir=None):
     whole_text_freq = corpus.sum()
     wc = WordCloud(max_font_size=300,min_font_size=30,
                max_words=1000,
@@ -172,7 +204,7 @@ def word_cloud_plot_no_mask(corpus,save_plot_dir):
     plt.close()
 
 # Build a word cloud without mask image
-def word_cloud_plot_no_mask(corpus,save_plot_dir):
+def word_cloud_plot_no_mask(corpus,save_plot_dir=None):
     whole_text_freq = corpus.sum()
     wc = WordCloud(max_font_size=300,min_font_size=30,
                max_words=1000,
@@ -220,7 +252,7 @@ def word_cloud_plot(mask_image_path, corpus,save_plot_dir):
 
 
 # PCA plot
-def pca_plot(classes, vecs,save_plot_dir):
+def pca_plot(classes, vecs,save_plot_dir=None):
     pca = PCA(n_components=2)
     reduced_vecs = pca.fit_transform(vecs)
     fig, ax = plt.subplots()
@@ -240,7 +272,7 @@ def pca_plot(classes, vecs,save_plot_dir):
     plt.close()
 
 # kmeans plot
-def kmeans_plot(classes, vecs,save_plot_dir):
+def kmeans_plot(classes, vecs,save_plot_dir=None):
     kmeans = KMeans(n_clusters=9).fit(vecs)
     c_labels = kmeans.labels_
     fig, ax = plt.subplots()
@@ -257,3 +289,83 @@ def kmeans_plot(classes, vecs,save_plot_dir):
     #image
     fig.savefig(figname, figdpi = 300)
     plt.close()
+
+
+def evaluate_features(X, y, clf):
+    """General helper function for evaluating effectiveness of passed features in ML model
+    
+    Prints out Log loss, accuracy, and confusion matrix with 3-fold stratified cross-validation
+    
+    Args:
+        X (array-like): Features array. Shape (n_samples, n_features)
+        
+        y (array-like): Labels array. Shape (n_samples,)
+        
+        clf: Classifier to use. If None, default Log reg is use.
+        e.g.
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.model_selection import StratifiedKFold
+        from sklearn.metrics import log_loss, accuracy_score
+        from sklearn.datasets import load_iris
+        clf = LogisticRegression()
+        evaluate_features(*load_iris(True),clf)
+    """
+    if clf is None:
+        pass
+    else:
+        probas = cross_val_predict(clf, X, y, cv=StratifiedKFold(random_state=8), \
+                                   n_jobs=-1, method='predict_proba', verbose=2)
+        pred_indices = np.argmax(probas, axis=1)
+        classes = np.unique(y)
+        preds = classes[pred_indices]
+        print('Log loss: {}'.format(log_loss(y, probas)))
+        print('Accuracy: {}'.format(accuracy_score(y, preds)))
+        skplt.plot_confusion_matrix(y, preds)
+
+
+# Split a pandas dataframe into train and validation dataset 
+def split_data(df,text,target,test_size,random_state,stratify=None):
+    '''
+    df[text]: text data for training
+    df[target]: label of text data
+    
+    '''
+    df[text] = df[text].astype(str)
+    df[target] = df[target].astype(str)
+    X= df[text].values
+    y =df[target].values
+    X_tr, X_val, y_tr, y_val = train_test_split(X,
+                                            y,
+                                            test_size=test_size,
+                                            stratify=df[target],
+                                            random_state=random_state)
+    return X_tr, X_val, y_tr, y_val
+
+# +
+
+
+def clean_text(t):
+    """Accepts a Document 
+    """
+    t = t.lower()
+    # Remove single characters
+    t = re.sub("[^A-Za-z0-9]"," ",t)
+    # Replace all numbers by a single char
+    t = re.sub("[0-9]+","#",t)   
+    return t
+
+def clean_text_stemmed(t):
+    """Accepts a Document 
+    """
+    t = t.lower()
+    # Remove single characters
+    t = re.sub("[^A-Za-z0-9]"," ",t)
+    # Replace all numbers by a single char
+    t = re.sub("[0-9]+","#",t)
+    stemmer = snowballstemmer.stemmer('english')
+    tfinal = " ".join(stemmer.stemWords(t.split()))    
+    return t
+
+# -
+
+
